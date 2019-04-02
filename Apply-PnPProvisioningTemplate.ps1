4cdb04671d963d6dbaca7059ad0f5aac48b6f84f
Garry Trinder <garry.trinder@live.com>
2019-04-02 23:56:07 +0100

    Added example of template passed to runbook

---
+++ Apply-PnPProvisioningTemplate.ps1
@@ -0,0 +1,23 @@
+Param(
+    [Parameter(Mandatory=$true)]
+    [string]$SiteUrl,
+    [Parameter(Mandatory=$true)]
+    [xml]$Template
+)
+
+$ErrorActionPreference = "Stop"
+
+# store XML as local file
+$Template.save("$home\template.xml")
+
+$AppId = Get-AutomationVariable -Name 'SharePointAppId'
+$AppSecret = Get-AutomationVariable -Name 'SharePointAppSecret'
+
+# create connection to SharePoint site
+$Connect = Connect-PnPOnline -AppId $AppId -AppSecret $AppSecret -Url $Url
+
+# apply template
+$ApplyPnPProvisioningTemplate = Apply-PnPProvisioningTemplate -Path "$home\template.xml" -Wait
+
+# clean up SharePoint connection
+$Disconnect = Disconnect-PnPOnline
